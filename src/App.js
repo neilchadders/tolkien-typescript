a7adacedd7323105b46b9fe80dfda1a56ff7d18a
Neil Chadwick <neilchadders@hotmail.com>
2023-01-23 07:32:24 +0000

    adding map function and key to array

--- src/App.js
+++ src/App.js
@@ -5,10 +5,28 @@ import './App.css';
 
 class App extends Component {
 
+  constructor() {    //access and generate the state. Constructor method runs before everything else
+    super();    //super calls the constructor method on Component
+
+    this.state = {
+      characters: [
+        { name: "Frodo", id: '1234' },
+        { name: "Sam", id: '12356' },
+        { name: "Merry", id: '1278' },
+        { name: "Pippin", id: '1234987' }
+      ],//if using lists / map - KEY WILL BE NEEDED
+    };
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        <h1>TEST TEST</h1>
+        {this.state.characters.map(character => {   //if using lists / map - KEY WILL BE NEEDED
+          return <h1 key={character.id}>{character.name}</h1> // what you return must be on same line
+        })
+        }
       </div>
     );
   }
